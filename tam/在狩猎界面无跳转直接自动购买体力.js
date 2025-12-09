9e052e7b3ad85c90c9b3c50501d48433d39252c9
MrWamG <wangliamgcom@gmail.com>
2025-12-09 17:10:36 +0800

    油猴自动补血脚本：增加了金币不够补血时终止狩猎； 狩猎时间大于 11 小时 30 分钟时回到狩猎列表并重新开始新一轮狩猎； 每 1 分 10 秒主动刷新页面，尽可能减少意外情况（如游戏维护）造成的脚本卡死影响。

--- tam/在狩猎界面无跳转直接自动购买体力.js
+++ tam/在狩猎界面无跳转直接自动购买体力.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vanya Online 自动花费金币购买体力
 // @namespace    http://tampermonkey.net/
-// @version      0.0.5
+// @version      0.0.6
 // @description  网页游戏 Vanya Online (https://vanyaonline.com/) 的自动化脚本
 // @author       王铁牛(QQ: 2459120212)
 // @icon         https://vanyaonline.com/favicon.ico
@@ -9,6 +9,80 @@
 // @include      *https://vanyaonline.com/*
 // @grant        none
 // ==/UserScript==
+
+const parseTimeString = (timeStr) => {
+    // 将字符串分割为时、分、秒部分
+    const parts = timeStr.split(/\s*,\s*/);
+    let totalSeconds = 0;
+
+    parts.forEach(part => {
+        if (part.endsWith('h')) {
+            totalSeconds += parseInt(part) * 3600;
+        } else if (part.endsWith('m')) {
+            totalSeconds += parseInt(part) * 60;
+        } else if (part.endsWith('s')) {
+            totalSeconds += parseInt(part);
+        }
+    });
+
+    return totalSeconds;
+}
+
+
+// 终止狩猎（狩猎时间即将超过 12 小时或没钱买血时执行）
+const huntStop = async () => {
+    fetch('https://vanyaonline.com/actions/hunt_stop', {
+        method: 'POST',
+        headers: {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,ja;q=0.7,zh-TW;q=0.6',
+            'cache-control': 'max-age=0',
+            'content-type': 'application/x-www-form-urlencoded',
+            'origin': 'https://vanyaonline.com',
+            'priority': 'u=0, i',
+            'referer': 'https://vanyaonline.com/actions/hunt',
+            'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
+        },
+        body: `action=stop_hunt&csrf_token=${globalCsrfToken}`,
+        credentials: 'include'  // 这会让浏览器自动发送 cookies
+    })
+        .then(response => response.text())
+        .then(data => console.log(data))
+        .catch(error => console.error('Error:', error));
+}
+
+// 监听狩猎时间是否已经大于 11 小时 30 分，是的话则终止本轮狩猎，开始准备下一轮狩猎
+const isTimeEnd = async () => {
+    if(window.location.pathname === "/actions/hunt"){
+        /* 格式是 1h,12m,30s */
+        const huntTime = document.getElementById("time-elapsed").innerHTML;
+        // 11小时30分钟的秒数
+        const threshold = 11 * 3600 + 30 * 60; // 41400秒
+        console.log("parseTimeString(huntTime)=>",parseTimeString(huntTime));
+        // 如果当前狩猎时间大于 11 小时 30 分钟则终止狩猎，然后开始下一轮狩猎
+        if(parseTimeString(huntTime) > threshold){
+            await huntStop();
+            setTimeout(() => {
+                // 进入狩猎列表
+                window.location = "https://vanyaonline.com/actions/explore";
+            },300)
+        }
+    }
+
+    // 进入了狩猎列表，开始找自己最高能打的本
+    if(window.location.pathname === "/actions/explore"){
+        const huntButtons = document.querySelectorAll('button[type*="submit"]');
+        huntButtons[huntButtons.length - 1].click();
+    }
+}
 
 // 抓取 https://vanyaonline.com/actions/hunt 中剩余的生命值
 const catchLife = async () => {
@@ -19,7 +93,7 @@ const catchLife = async () => {
         // 如果生命值小于阈值（limit）则直接购买体力（不需要去酒馆，我直接调用接口）
         if(life <= limit) {
             try {
-                await fetch(
+                const result = await fetch(
                     "https://vanyaonline.com/process/pub_npc_buy.php",
                     {
                         method: "POST",
@@ -43,7 +117,10 @@ const catchLife = async () => {
                         credentials: "include", // 相当于 axios 的 withCredentials: true
                     }
                 );
-                
+                // 如果购买生命值没成功则终止狩猎
+                if(result.status === "error"){
+                    huntStop();
+                }
             } catch (error) {
                 console.error("Error:", error.message);
             }
@@ -53,5 +130,13 @@ const catchLife = async () => {
 
 // 每三秒检测一次
 setInterval(async () => {
+    await isTimeEnd();
     await catchLife();
 },1000 * 3)
+
+// 游戏在刷怪界面是每过 1 分钟刷新一次页面以更新数据，为了避免发生意外情况（如果游戏维护）导致游戏不刷新内容，故我方每 1分10秒 刷新一次页面
+setInterval(async () => {
+    if(window.location.pathname === "/actions/hunt"){
+        location.reload();
+    };
+},1000 * 70)
